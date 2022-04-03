c4545d05fed72312547cfd029325eae37ba59a8d
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-03 16:54:11 +0200

    Finish add button unctionality

--- js/site.js
+++ js/site.js
@@ -1,3 +1,46 @@
+function PrepareCartItem(droppedItem) {
+    droppedItem.find('.btn-remove').addClass('d-inline-block');
+    droppedItem.find('.btn-remove').removeClass('d-none');
+    droppedItem.find('.btn-add').addClass('d-none');
+    droppedItem.find('.btn-add').removeClass('d-block');
+    droppedItem.find('.card-price-info').removeClass('d-none');
+    droppedItem.find('.card-price-info').addClass('d-block');
+    droppedItem.find('.card-count').addClass('d-block');
+    droppedItem.find('.card-count').removeClass('d-none');
+}
+
+function AddingToCart(cart) {
+    $('#Cart').droppable({
+        accept: ".card",
+        drop: function (event, target) {
+            let droppedItem = $(target.draggable).clone();
+
+            console.log(droppedItem);
+            droppedItem.find('.btn-remove').on('click', (event) => {
+                console.log(event);
+            });
+            PrepareCartItem(droppedItem);
+
+            if (cart.findIndex(value => {
+                    return value.id === droppedItem[0].id;
+                }
+            ) > -1) {
+                let index = cart.findIndex(value => {
+                        return value.id === droppedItem[0].id;
+                    }
+                )
+                cart[index].count++;
+                $("#Cart" + ' #' + cart[index].id + " .card-count")[0].lastChild.textContent = cart[index].count;
+
+            } else {
+                cart.push({id: droppedItem[0].id, count: 1});
+                console.log(cart);
+                $(this).append(droppedItem);
+            }
+        }
+    });
+}
+
 $(document).ready(function () {
 
     let productsArea = $('#Products');
@@ -50,46 +93,34 @@ $(document).ready(function () {
                 cursor: "move",
             });
 
-            $('#Cart').droppable({
-                accept: ".card",
-                drop: function (event, target) {
-                    let droppedItem = $(target.draggable).clone();
+            AddingToCart(cart);
 
-                    console.log(droppedItem);
-                    droppedItem.find('.btn-remove').on('click', (event) => {
-                        console.log(event);
-                    });
+            $('.card').on("click", ".btn-add", function (target) {
 
-                    droppedItem.find('.btn-remove').addClass('d-inline-block');
-                    droppedItem.find('.btn-remove').removeClass('d-none');
-                    droppedItem.find('.btn-add').addClass('d-none');
-                    droppedItem.find('.btn-add').removeClass('d-block');
-                    droppedItem.find('.card-price-info').removeClass('d-none');
-                    droppedItem.find('.card-price-info').addClass('d-block');
-                    droppedItem.find('.card-count').addClass('d-block');
-                    droppedItem.find('.card-count').removeClass('d-none');
+                let clone = target.currentTarget.parentElement.cloneNode(true);
+                clone.childNodes[13].classList.remove('d-none');
+                clone.childNodes[13].classList.add('d-inline-block');
+                clone.childNodes[11].classList.add('d-none');
+                clone.childNodes[9].classList.add('d-block');
+                clone.childNodes[9].classList.remove('d-none');
 
-                    if (cart.findIndex(value => {
-                            return value.id === droppedItem[0].id;
-                        }
-                    ) > -1) {
-                        let index = cart.findIndex(value => {
-                                return value.id === droppedItem[0].id;
-                            }
-                        )
-                        cart[index].count++;
-                        $("#Cart" + ' #' + cart[index].id + " .card-count")[0].lastChild.textContent = cart[index].count;
-
-                    } else {
-                        cart.push({id: droppedItem[0].id, count: 1});
-                        console.log(cart);
-                        $(this).append(droppedItem);
+                console.log(clone.id);
+                if (cart.findIndex(value => {
+                        return value.id === clone.id;
                     }
-                }
-            });
+                ) > -1) {
+                    let index = cart.findIndex(value => {
+                            return value.id === clone.id;
+                        }
+                    )
+                    cart[index].count++;
+                    $("#Cart" + ' #' + cart[index].id + " .card-count")[0].lastChild.textContent = cart[index].count;
 
-            $('.card').on("click", ".btn-add", function () {
-                console.log("Fired");
+                } else {
+                    cart.push({id: clone.id, count: 1});
+                    console.log(cart);
+                    $("#Cart").append(clone);
+                }
             });
         }
     });
